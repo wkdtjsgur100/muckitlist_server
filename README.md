9e9fdaf6bc5065843fc5e9bb913cb6b381d57163
Ubuntu <ubuntu@ip-172-31-9-167.ap-northeast-2.compute.internal>
2016-08-29 17:55:04 +0000

    register DB on admin, add readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# 먹킷리스트 
+
+TO DO LIST
+
+ - instagram crawl & parsing
+ - kakaotalk, fb login & sharing
--- muckitlist/admin.py
+++ muckitlist/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Publisher
+
+admin.site.register(Publisher)
+
 
 # Register your models here.
